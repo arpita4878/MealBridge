1d06b4178b9bb2a8fe2f2e2b364d243e1839e583
arpita4878 <arpitaseth192@gmail.com>
2025-08-07 14:39:54 +0530

    "Chatbot implement"

--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -1,28 +1,18 @@
-import express from 'express'
-import * as UserController from '../controller/user.controller.js'
+import express from 'express';
+import * as AuthController from '../controller/auth.controller.js';
+import * as UserController from '../controller/user.controller.js';
 
-const router=express.Router()
+const router = express.Router();
 
-router.post("/login",UserController.login)
-router.post("/google-login", UserController.googleLogin);
-router.get('/check-email', UserController.checkEmail);
+// Auth routes
+router.post("/login", AuthController.login);
+router.post("/register", AuthController.register);
+router.post("/google-login", AuthController.googleLogin);
 
-router.post("/register",UserController.register)
-
-router.patch("/update",UserController.update)
-
-router.get("/fetch",UserController.fetch)
-
-router.delete("/deleteUser",UserController.deleteUser)
+// User management routes
+router.get("/check-email", UserController.checkEmail);
+router.patch("/update", UserController.update);
+router.get("/fetch", UserController.fetch);
+router.delete("/deleteUser", UserController.deleteUser);
 
 export default router;
-
-
-
-
-
-
-
-
-
-
