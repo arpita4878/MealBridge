74cfc75963e2abb1607759027025137f5310a2ae
arpita4878 <arpitaseth192@gmail.com>
2025-08-07 15:20:23 +0530

    "implement FoodBot is typing..."

--- food/src/Api_Url.jsx
+++ food/src/Api_Url.jsx
@@ -2,4 +2,4 @@ export const __userapiurl = "https://mealbridge-qemx.onrender.com/user/";
 export const __foodapiurl = "https://mealbridge-qemx.onrender.com/food/";
 export const __volunteerapiurl = "https://mealbridge-qemx.onrender.com/volunteer/";
 export const __partnerapiurl = "https://mealbridge-qemx.onrender.com/partner/";
-export const __chatbotapiurl = "https://mealbridge-qemx.onrender.com/api/chatbot"; // for chatbot post requests
+export const __chatbotapiurl = "https://mealbridge-qemx.onrender.com/api/"; // for chatbot post requests
